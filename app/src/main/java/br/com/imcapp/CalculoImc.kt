1457c42173e70f53ea6be2da0b018e7f355dd280
Dante <dante.campos.barros2@outlook.com>
2025-03-04 16:26:03 -0300

    IMC app vs1

---
+++ app/src/main/java/br/com/imcapp/CalculoImc.kt
@@ -0,0 +1,21 @@
+package br.com.fiap.imcapp
+
+import kotlin.math.pow
+
+fun calcularImc(alturaUsuario: Double, pesoUsuario: Double) : Double {
+    return pesoUsuario / (alturaUsuario / 100).pow(2.0)
+}
+
+fun obterStatusImc(imc: Double): String {
+    return if(imc < 18.5) {
+        "Abaixo do peso"
+    } else if (imc >= 18.5 && imc < 25.0) {
+        "Peso Ideal"
+    } else if (imc >= 25.0 && imc < 30.0) {
+        "Levemente acima do peso"
+    } else if (imc >= 30.0 && imc < 35.0) {
+        "Obesidade Grau I"
+    } else if (imc >= 35.0 && imc < 40.0) {
+        "Obesidade Grau II"
+    } else {"Obesidade Grau III"}
+}
